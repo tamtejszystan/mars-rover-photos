a961a592c8c52168d25866236cbf1b789a618f4e
tamtejszystan <mtustanowski@gmail.com>
2023-02-23 23:36:13 +0100

    mars-rover-photos service init

--- src/main/java/com/example/marsRoverPhotos/service/RoverService.java
+++ src/main/java/com/example/marsRoverPhotos/service/RoverService.java
@@ -1,4 +1,25 @@
 package com.example.marsRoverPhotos.service;
 
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Getter
 public class RoverService {
+
+    private static final String API_KEY = "4KyyHycAvnywP1DvJgKWjnJ7kc18GCygZTf9MpNr";
+
+    private Map<String, List<String>> validCameras = new HashMap<>();
+
+    public RoverService() {
+        validCameras.put("Opportunity", Arrays.asList("FHAZ", "RHAZ", "NAVCAM", "PANCAM", "MINITES"));
+        validCameras.put("Curiosity", Arrays.asList("FHAZ", "RHAZ", "MAST", "CHEMCAM", "MAHLI", "MARDI", "NAVCAM"));
+        validCameras.put("Spirit", Arrays.asList("FHAZ", "RHAZ", "NAVCAM", "PANCAM", "MINITES"));
+    }
+
 }
