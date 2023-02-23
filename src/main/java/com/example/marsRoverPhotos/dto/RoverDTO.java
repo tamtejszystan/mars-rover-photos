68e8be719f8357237887ceb50f7b3eb6fa8f8983
tamtejszystan <mtustanowski@gmail.com>
2023-02-23 23:43:46 +0100

    mars-rover-photos roverDTO

--- src/main/java/com/example/marsRoverPhotos/dto/RoverDTO.java
+++ src/main/java/com/example/marsRoverPhotos/dto/RoverDTO.java
@@ -1,4 +1,27 @@
 package com.example.marsRoverPhotos.dto;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class RoverDTO {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String roverType;
+    private Integer marsSol;
+    private Boolean cameraFhaz;
+    private Boolean cameraRhaz;
+    private Boolean cameraMast;
+    private Boolean cameraChemcam;
+    private Boolean cameraMahli;
+    private Boolean cameraMardi;
+    private Boolean cameraNavcam;
+    private Boolean cameraPancam;
+    private Boolean cameraMinites;
 }
