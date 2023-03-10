60a3bdc6004eede4e473f4eb5571f9aed6630075
tamtejszystan <mtustanowski@gmail.com>
2023-03-10 19:10:31 +0100

    mars-rover-photos bug-fix

--- src/main/java/com/example/marsRoverPhotos/response/MarsPhoto.java
+++ src/main/java/com/example/marsRoverPhotos/response/MarsPhoto.java
@@ -1,11 +1,9 @@
 package com.example.marsRoverPhotos.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@AllArgsConstructor
 @Getter
 @Setter
 public class MarsPhoto {
